317279e81f31ff1713716904d1c9dc607a7ae5a4
Bruce Song <recall4056@gmail.com>
2022-08-15 00:36:17 +0800

    A cors config test.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
 
 type Data = {
-  name: string
+  name: string;
+};
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await runMiddleware(req, res, cors);
+  res.status(200).json({ name: "John Doe" });
 }
