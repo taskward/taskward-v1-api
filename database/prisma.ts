e2d42425e5743ab3b198e4a3fb2cd5c432b48fed
Bruce Song <recall4056@gmail.com>
2022-08-16 11:14:47 +0800

    Prisma instance configuration.

---
+++ database/prisma.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+interface CustomNodeJSGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+declare const global: CustomNodeJSGlobal;
+
+const prisma: PrismaClient =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+    errorFormat: "pretty",
+  });
+
+// Logging middleware
+prisma.$use(async (params: any, next: any) => {
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+  console.log(
+    `Query ${params.model}.${params.action} took ${after - before}ms`
+  );
+  return result;
+});
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export { prisma };
