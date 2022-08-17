10efa0b2b5a322519d903d5d60b551728412aee7
Bruce Song <recall4056@gmail.com>
2022-08-18 00:50:59 +0800

    Update schema.

--- database/prisma.ts
+++ database/prisma.ts
@@ -24,6 +24,75 @@ prisma.$use(async (params: any, next: any) => {
   return result;
 });
 
+// Soft delete middleware
+prisma.$use(async (params: any, next: any) => {
+  if (params.model == "Post") {
+    if (params.action === "findUnique" || params.action === "findFirst") {
+      // Change to findFirst - you cannot filter
+      // by anything except ID / unique with findUnique
+      params.action = "findFirst";
+      // Add 'deleted' filter
+      // ID filter maintained
+      params.args.where["deleted"] = false;
+    }
+    if (params.action === "findMany") {
+      // Find many queries
+      if (params.args.where) {
+        if (params.args.where.deleted == undefined) {
+          // Exclude deleted records if they have not been explicitly requested
+          params.args.where["deleted"] = false;
+        }
+      } else {
+        params.args["where"] = { deleted: false };
+      }
+    }
+  }
+  return next(params);
+});
+
+prisma.$use(async (params: any, next: any) => {
+  if (params.model == "Post") {
+    if (params.action == "update") {
+      // Change to updateMany - you cannot filter
+      // by anything except ID / unique with findUnique
+      params.action = "updateMany";
+      // Add 'deleted' filter
+      // ID filter maintained
+      params.args.where["deleted"] = false;
+    }
+    if (params.action == "updateMany") {
+      if (params.args.where != undefined) {
+        params.args.where["deleted"] = false;
+      } else {
+        params.args["where"] = { deleted: false };
+      }
+    }
+  }
+  return next(params);
+});
+
+prisma.$use(async (params: any, next: any) => {
+  // Check incoming query type
+  if (params.model == "Post") {
+    if (params.action == "delete") {
+      // Delete queries
+      // Change action to an update
+      params.action = "update";
+      params.args["data"] = { deleted: true };
+    }
+    if (params.action == "deleteMany") {
+      // Delete many queries
+      params.action = "updateMany";
+      if (params.args.data != undefined) {
+        params.args.data["deleted"] = true;
+      } else {
+        params.args["data"] = { deleted: true };
+      }
+    }
+  }
+  return next(params);
+});
+
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
