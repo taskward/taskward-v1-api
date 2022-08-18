b63f3a574658861cd7d17b3667541b62d210994c
Bruce Song <recall4056@gmail.com>
2022-08-18 13:43:54 +0800

    Update scheme

--- prisma/migrations/20220818054247_init/migration.sql
+++ prisma/migrations/20220818054247_init/migration.sql
@@ -7,13 +7,13 @@ CREATE TYPE "auth_type" AS ENUM ('username', 'github');
 -- CreateTable
 CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "update_at" TIMESTAMP(3),
-    "deleteAt" TIMESTAMP(3),
+    "delete_at" TIMESTAMP(3),
     "username" VARCHAR(25) NOT NULL,
     "email" VARCHAR(25) NOT NULL,
     "name" VARCHAR(25),
-    "avatarUrl" TEXT,
+    "avatar_url" TEXT,
     "biography" VARCHAR(50),
     "location" VARCHAR(25),
     "role" "role" NOT NULL DEFAULT 'user',
@@ -24,11 +24,11 @@ CREATE TABLE "user" (
 -- CreateTable
 CREATE TABLE "oauth" (
     "id" SERIAL NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "deleteAt" TIMESTAMP(3),
-    "authType" "auth_type" NOT NULL DEFAULT 'username',
-    "openId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+    "auth_type" "auth_type" NOT NULL DEFAULT 'username',
+    "open_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
 
     CONSTRAINT "oauth_pkey" PRIMARY KEY ("id")
 );
@@ -36,17 +36,17 @@ CREATE TABLE "oauth" (
 -- CreateTable
 CREATE TABLE "note" (
     "id" SERIAL NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "update_at" TIMESTAMP(3),
-    "completeAt" TIMESTAMP(3),
-    "deleteAt" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "finished_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
     "name" VARCHAR(50) NOT NULL,
     "description" VARCHAR(255),
     "priority" INTEGER DEFAULT 0,
     "index" INTEGER NOT NULL DEFAULT 0,
-    "top" BOOLEAN NOT NULL DEFAULT false,
-    "archieve" BOOLEAN NOT NULL DEFAULT false,
-    "userId" INTEGER NOT NULL,
+    "toped" BOOLEAN NOT NULL DEFAULT false,
+    "archieved" BOOLEAN NOT NULL DEFAULT false,
+    "user_id" INTEGER NOT NULL,
 
     CONSTRAINT "note_pkey" PRIMARY KEY ("id")
 );
@@ -54,11 +54,11 @@ CREATE TABLE "note" (
 -- CreateTable
 CREATE TABLE "task" (
     "id" SERIAL NOT NULL,
-    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "update_at" TIMESTAMP(3),
-    "complete_at" TIMESTAMP(3),
-    "delete_at" TIMESTAMP(3),
-    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "finished_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+    "finished" BOOLEAN NOT NULL DEFAULT false,
     "content" VARCHAR(255),
     "link_url" TEXT,
     "index" INTEGER NOT NULL DEFAULT 0,
@@ -70,8 +70,8 @@ CREATE TABLE "task" (
 -- CreateTable
 CREATE TABLE "tag" (
     "id" SERIAL NOT NULL,
-    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "delete_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
     "index" INTEGER NOT NULL DEFAULT 0,
     "user_id" INTEGER NOT NULL,
     "note_id" INTEGER NOT NULL,
@@ -82,8 +82,8 @@ CREATE TABLE "tag" (
 -- CreateTable
 CREATE TABLE "theme" (
     "id" SERIAL NOT NULL,
-    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "delete_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
     "index" INTEGER NOT NULL DEFAULT 0,
     "user_id" INTEGER NOT NULL,
     "note_id" INTEGER NOT NULL,
@@ -98,13 +98,13 @@ CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
 CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "oauth_openId_key" ON "oauth"("openId");
+CREATE UNIQUE INDEX "oauth_open_id_key" ON "oauth"("open_id");
 
 -- AddForeignKey
-ALTER TABLE "oauth" ADD CONSTRAINT "oauth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "oauth" ADD CONSTRAINT "oauth_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "note" ADD CONSTRAINT "note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "note" ADD CONSTRAINT "note_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "task" ADD CONSTRAINT "task_note_id_fkey" FOREIGN KEY ("note_id") REFERENCES "note"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
