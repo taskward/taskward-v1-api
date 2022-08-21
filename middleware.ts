8449b3e1482d3d2cbcb537f8839ed1b89e3ca5eb
Bruce Song <recall4056@gmail.com>
2022-08-21 12:42:19 +0800

    Update.

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { validateToken } from "@utils";
+import type { NextResponseWithAuth } from "@interfaces";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    process.env.TASKWARD_BASE_URL
+  );
   response.headers.set(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
   response.headers.set(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Authentication, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
   return response;
 }
