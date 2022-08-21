3c3e22a45dced972330f8d0ddf82efdf61c249ed
Bruce Song <recall4056@gmail.com>
2022-08-21 12:59:29 +0800

    Update.

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,10 @@ import type { NextResponseWithAuth } from "@interfaces";
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    process.env.TASKWARD_BASE_URL
+  );
   response.headers.set(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
