e040b7c115ecae3ca5cd1a3720691415bafcae10
Bruce Song <recall4056@gmail.com>
2022-08-21 14:10:20 +0800

    Update jwt method.

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest } from "next";
-import { sign, verify, decode } from "jsonwebtoken";
+import { sign, verify, decode, JwtPayload } from "jsonwebtoken";
 
 import { User } from "@prisma/client";
 
@@ -7,29 +7,35 @@ function createToken(user: User): string {
   const accessToken = sign(
     { username: user.username, id: user.id, role: user.role },
     process.env.JWT_KEY,
-    { expiresIn: "1d" }
+    { expiresIn: "1h" }
   );
   return accessToken;
 }
 
 function validateToken(request: NextApiRequest): boolean {
-  const accessToken = request.headers.authorization?.split(" ")[1];
+  const accessToken = request.headers?.authorization?.split(" ")[1];
   if (!accessToken) {
     return false;
   }
-  const validToken = verify(accessToken, process.env.JWT_KEY);
-  if (!validToken) {
+  try {
+    const validToken = verify(accessToken, process.env.JWT_KEY);
+    if (!validToken) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.error(error);
     return false;
   }
-  return true;
 }
 
-function decodeToken(request: NextApiRequest) {
-  const accessToken = request.headers.authorization?.split(" ")[1];
+function decodeToken(request: NextApiRequest): string | JwtPayload | null {
+  const accessToken = request.headers?.authorization?.split(" ")[1];
   if (!accessToken) {
-    return false;
+    return null;
   }
-  const result = decode(accessToken);
+  const decodeResult = decode(accessToken);
+  return decodeResult;
 }
 
-export { createToken, validateToken };
+export { createToken, validateToken, decodeToken };
