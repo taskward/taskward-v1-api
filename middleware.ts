32fb6cb06a49f2f46152c4b1c7959b56c16a38c0
Bruce Song <recall4056@gmail.com>
2022-08-21 23:30:54 +0800

    Update.

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  if (request.method === "OPTIONS") {
-    return new Response();
-  }
   response.headers.set("Access-Control-Allow-Credentials", "true");
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set(
@@ -16,5 +13,7 @@ export async function middleware(request: NextRequest) {
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
+  response.headers.set("Connection", "keep-alive");
+  response.headers.set("Keep-Alive", "timeout=5");
   return response;
 }
