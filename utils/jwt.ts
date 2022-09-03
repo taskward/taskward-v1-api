03667dacbf9145cb424d3b0c0b9145d5a15820d6
Bruce Song <recall4056@gmail.com>
2022-09-03 14:11:52 +0800

    Update.

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -10,7 +10,7 @@ function createToken(user: JWTUserModel): string | null {
   const accessToken = sign(
     { username: user.username, userId: user.userId, role: user.role },
     process.env.JWT_KEY,
-    { expiresIn: "1h" }
+    { expiresIn: "1d" }
     // { expiresIn: "1s" }
   );
   return accessToken;
