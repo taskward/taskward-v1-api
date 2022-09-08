98d15d849dea759dc458bb5fda4fd503c9324624
Bruce <recall4056@gmail.com>
2022-09-08 21:09:55 +0800

    Update prisma.

---
+++ prisma/migrations/20220908130902_remove_task_finished_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `finished` on the `task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "task" DROP COLUMN "finished";
