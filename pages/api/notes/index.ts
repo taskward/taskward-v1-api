0aab05f65d5e733a886701eeba89d5a0cee44bbb
Bruce <recall4056@gmail.com>
2022-09-08 21:54:50 +0800

    Update API.

--- pages/api/notes/index.ts
+++ pages/api/notes/index.ts
@@ -50,19 +50,45 @@ const handler = async (
 
       response.status(200).json({ notes: notes, count: notes.length });
     } else if (request.method === "POST") {
-      const { name, description } = request.body;
+      const { name, description, tasks: tasksData } = request.body;
 
-      const note = await prisma.note.create({
-        data: {
-          name: name,
-          description: description,
-          userId: authResult.userId,
-        },
-      });
-
-      if (!note) {
-        response.status(404).end();
-        return;
+      // Check whether the note has tasks
+      if (tasksData.length > 0) {
+        const tasks = tasksData.map((task: any) => {
+          return {
+            content: task.content,
+            linkUrl: task.linkUrl,
+            finishedAt: task.checked ? new Date().toISOString() : null,
+          };
+        });
+        const note = await prisma.note.create({
+          data: {
+            name: name,
+            description: description,
+            userId: authResult.userId,
+            tasks: {
+              createMany: {
+                data: tasks,
+              },
+            },
+          },
+        });
+        if (!note) {
+          response.status(404).end();
+          return;
+        }
+      } else {
+        const note = await prisma.note.create({
+          data: {
+            name: name,
+            description: description,
+            userId: authResult.userId,
+          },
+        });
+        if (!note) {
+          response.status(404).end();
+          return;
+        }
       }
 
       response.status(200).json({
